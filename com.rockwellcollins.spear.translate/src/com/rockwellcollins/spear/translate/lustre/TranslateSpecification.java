ebaecfe113f1497df44ec61300ab8392451405a1
Lucas Wagner <lgwagner@gmail.com>
2016-01-29 16:05:45 -0600

    commit

--- com.rockwellcollins.spear.translate/src/com/rockwellcollins/spear/translate/lustre/TranslateSpecification.java
+++ com.rockwellcollins.spear.translate/src/com/rockwellcollins/spear/translate/lustre/TranslateSpecification.java
@@ -1,14 +1,25 @@
 package com.rockwellcollins.spear.translate.lustre;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.eclipse.emf.common.util.EList;
+
+import com.rockwellcollins.spear.Constraint;
+import com.rockwellcollins.spear.FormalConstraint;
+import com.rockwellcollins.spear.Macro;
 import com.rockwellcollins.spear.Specification;
 
 import jkind.lustre.Constant;
+import jkind.lustre.Equation;
+import jkind.lustre.Expr;
+import jkind.lustre.IdExpr;
+import jkind.lustre.NamedType;
 import jkind.lustre.Program;
+import jkind.lustre.Type;
 import jkind.lustre.TypeDef;
 import jkind.lustre.VarDecl;
 
@@ -45,7 +56,7 @@ public class TranslateSpecification {
 		return unique;
 	}
 	
-	private List<TypeDef> getSpecificationTypeDefs(Specification s) {
+	private List<TypeDef> getTypeDefs(Specification s) {
 		List<TypeDef> typedefs = new ArrayList<>();
 		for(com.rockwellcollins.spear.TypeDef spearTypeDef : s.getTypedefs()) {
 			TypeDef lustreTypeDef = TranslateTypeDef.translate(spearTypeDef);
@@ -55,7 +66,7 @@ public class TranslateSpecification {
 		return typedefs;
 	}
 	
-	private List<Constant> getSpecificationConstants(Specification s) {
+	private List<Constant> getConstants(Specification s) {
 		List<Constant> constants = new ArrayList<>();
 		for(com.rockwellcollins.spear.Constant spearConstant : s.getConstants()) {
 			Constant lustreConstant = TranslateSpecification.translate(spearConstant);
@@ -65,7 +76,7 @@ public class TranslateSpecification {
 		return constants;
 	}
 	
-	private List<VarDecl> getSpecificationVariables(List<com.rockwellcollins.spear.Variable> variables) {
+	private List<VarDecl> getVariables(List<com.rockwellcollins.spear.Variable> variables) {
 		List<VarDecl> list = new ArrayList<>();
 		for(com.rockwellcollins.spear.Variable var : variables) {
 			VarDecl lustreVariable = TranslateSpecification.translate(var);
@@ -76,7 +87,7 @@ public class TranslateSpecification {
 	}
 	
 	private List<VarDecl> createShadowVariables(List<VarDecl> variables) {
-		ArrayList<VarDecl> shadowVariables = new ArrayList<>();
+		List<VarDecl> shadowVariables = new ArrayList<>();
 		for(VarDecl v : variables) {
 			String name = getUniqueLocalName(v.id + SHADOW_SUFFIX);
 			shadowVariables.add(new VarDecl(name,v.type));
@@ -84,25 +95,96 @@ public class TranslateSpecification {
 		return shadowVariables;
 	}
 
+	private List<VarDecl> getMacrosVarDecls(List<Macro> macros) {
+		List<VarDecl> list = new ArrayList<>();
+		for(com.rockwellcollins.spear.Macro macro : macros) {
+			Type t = TranslateType.translate(macro.getType());
+			String name = getUniqueLocalName(macro.getName());
+			list.add(new VarDecl(name,t));
+		}
+		return list;
+	}
+	
+	private List<Equation> makeEquationsForShadowVars(List<VarDecl> state, List<VarDecl> shadowState) {
+		List<Equation> equations = new ArrayList<>();
+		for(int i=0; i<state.size(); i++) {
+			Equation eq = new Equation(Collections.singletonList(new IdExpr(state.get(i).id)),new IdExpr(shadowState.get(i).id));
+			equations.add(eq);
+		}
+		return equations;
+	}
+	
+	private List<VarDecl> getConstraintVarDecls(List<Constraint> constraints) {
+		List<VarDecl> constraintDecls = new ArrayList<>();
+		for(Constraint c : constraints) {
+			if (c instanceof FormalConstraint) {
+				FormalConstraint fc = (FormalConstraint) c;
+				constraintDecls.add(new VarDecl(fc.getName(), NamedType.BOOL));
+			}
+		}
+		return constraintDecls;
+	}
+	
+	private List<Equation> makeEquationsForConstraints(EList<Constraint> constraints) {
+		List<Equation> equations = new ArrayList<>();
+		for(Constraint c : constraints) {
+			if (c instanceof FormalConstraint) {
+				FormalConstraint fc = (FormalConstraint) c;
+				List<IdExpr> lhs = Collections.singletonList(new IdExpr(c.getName()));
+				Expr rhs = TranslateExpr.translate(fc.getExpr());
+				equations.add(new Equation(lhs,rhs));
+			}
+		}
+		return equations;
+	}
 	
 	public Program translate(Specification s) {
-		List<TypeDef> typedefs = getSpecificationTypeDefs(s);
-		List<Constant> constants = getSpecificationConstants(s);
+		String mainNodeName = s.getName();
+		List<TypeDef> typedefs = getTypeDefs(s);
+		List<Constant> constants = getConstants(s);
 		
 		//these represent the original inputs, outputs, and locals of a SpeAR specification
-		List<VarDecl> inputs = getSpecificationVariables(s.getInputs());
-		List<VarDecl> outputs = getSpecificationVariables(s.getOutputs());
-		List<VarDecl> state = getSpecificationVariables(s.getState());
+		List<VarDecl> inputs = getVariables(s.getInputs());
+		List<VarDecl> outputs = getVariables(s.getOutputs());
+		List<VarDecl> state = getVariables(s.getState());
 		
 		/*
-		 * These will be used to create nondeterministic inputs for the node
+		 * These will be used to create nondeterministic inputs for all the computed values in a Specification
 		 */
-		List<VarDecl> shadowOutputs = createShadowVariables(outputs);
 		List<VarDecl> shadowState = createShadowVariables(state);
-		
+		List<VarDecl> shadowOutputs = createShadowVariables(outputs);
+				
 		List<VarDecl> nodeInputs = new ArrayList<>();
 		nodeInputs.addAll(inputs);
+		nodeInputs.addAll(shadowState);
 		nodeInputs.addAll(shadowOutputs);
+		
+		List<VarDecl> macroVarDecls = getMacrosVarDecls(s.getMacros());
+		List<VarDecl> assumptionVarDecls = getConstraintVarDecls(s.getAssumptions());
+		List<VarDecl> behaviorVarDecls = getConstraintVarDecls(s.getBehaviors());
+		List<VarDecl> requirementVarDecls = getConstraintVarDecls(s.getRequirements());
+
+		List<VarDecl> nodeLocals = new ArrayList<>();
+		nodeLocals.addAll(state);
+		nodeLocals.addAll(macroVarDecls);
+		nodeLocals.addAll(assumptionVarDecls);
+		nodeLocals.addAll(behaviorVarDecls);
+		nodeLocals.addAll(requirementVarDecls);
+		
+		List<VarDecl> nodeOutputs = new ArrayList<>();
+		nodeOutputs.addAll(outputs);
+		
+		List<Equation> equations = new ArrayList<>();
+		equations.addAll(makeEquationsForShadowVars(state,shadowState));
+		equations.addAll(makeEquationsForShadowVars(outputs,shadowOutputs));
+		equations.addAll(makeEquationsForConstraints(s.getAssumptions()));
+		equations.addAll(makeEquationsForConstraints(s.getBehaviors()));
+		equations.addAll(makeEquationsForConstraints(s.getRequirements()));
+		
+		Equation conjunct = conjunctAssumptionsAndBehaviors(assumptionVarDecls,behaviorVarDecls);
+		equations.add(conjunct);
+		
+		
 		
 		
 
@@ -111,10 +193,8 @@ public class TranslateSpecification {
 		return null;
 	}
 
-
-
-
-
-
+	private Equation conjunctAssumptionsAndBehaviors(List<VarDecl> assumptionVarDecls, List<VarDecl> behaviorVarDecls) {
+		return null;
+	}
 
 }
