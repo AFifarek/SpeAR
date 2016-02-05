22e183eb16a1381e0c2a82e296cecb58dde200f5
Lucas Wagner <lgwagner@gmail.com>
2016-02-05 09:20:29 -0600

    Several changes:
    - added JXL library for viewing CEX in Excel.
    - fixed the Counterexample View in Eclipse

--- com.rockwellcollins.spear.translate/src/com/rockwellcollins/spear/translate/views/JKindCounterexampleView.java
+++ com.rockwellcollins.spear.translate/src/com/rockwellcollins/spear/translate/views/JKindCounterexampleView.java
@@ -11,7 +11,7 @@ import jkind.results.layout.Layout;
  * JKindCounterexampleView is a view.
  */
 public class JKindCounterexampleView extends ViewPart {
-	public static final String ID = "com.rockwellcollins.atc.limp.translate.lustre.views.jKindCounterexampleView";
+	public static final String ID = "com.rockwellcollins.spear.translate.views.jKindCounterexampleView";
 
 	private CounterexampleTreeViewer tree;
 
